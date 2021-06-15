efcafbb262f465283f578a5cae45a9c749f8837d
mix3@ｻﾀﾃﾞｰﾅｲﾄﾌｨｰﾊﾞｰ <himachocost333@hotmail.co.jp>
2021-06-15 16:25:50 +0900

    update tenco v0.1.0 -> v0.2.0

--- tenco.rb
+++ tenco.rb
@@ -1,13 +1,13 @@
 class Tenco < Formula
-  version '0.1.0'
+  version '0.2.0'
   homepage 'https://github.com/mix3/tenco'
   if OS.mac?
-    url "https://github.com/mix3/tenco/releases/download/v0.1.0/tenco-v0.1.0_darwin_amd64.zip"
-    sha256 '4e11d47e4f1fa2c804a63292ae6cc463508a1095ff05a0e164e1852be06793d4'
+    url "https://github.com/mix3/tenco/releases/download/v0.2.0/tenco-v0.2.0_darwin_amd64.zip"
+    sha256 '3e35314aed156df78e6be8facbee765b3f20f85691997057c650b7cf555d0115'
   end
   if OS.linux?
-    url "https://github.com/mix3/tenco/releases/download/v0.1.0/tenco-v0.1.0_linux_amd64.zip"
-    sha256 '0f76c9b01110e079051ed157deec21ff5b1201224cbdda8b9a735d5122686d74'
+    url "https://github.com/mix3/tenco/releases/download/v0.2.0/tenco-v0.2.0_linux_amd64.zip"
+    sha256 'afe62e68eedebd08eacba81457993c7a313b493075c89fc3c35651b149fc645e'
   end
   head 'https://github.com/mix3/tenco.git'
 
