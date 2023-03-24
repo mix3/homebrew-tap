23fc14e62be3a26d3dd7964a9348f3d47e02d629
mix3@ｻﾀﾃﾞｰﾅｲﾄﾌｨｰﾊﾞｰ <himachocost333@gmail.com>
2023-03-25 03:22:18 +0900

    use service section

--- activemonitor.rb
+++ activemonitor.rb
@@ -5,31 +5,13 @@ class Activemonitor < Formula
   sha256 'd332757d80fcf071d74c7d22c6ca4b96036c2fec40e38ef5af0b19889ac333ed'
   head 'https://github.com/mix3/activemonitor.git'
 
-  plist_options manual: "activemonitor"
-
-  def plist
-    <<~EOS
-      <?xml version="1.0" encoding="UTF-8"?>
-      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-      <plist version="1.0">
-      <dict>
-          <key>Label</key>
-          <string>#{plist_name}</string>
-          <key>ProgramArguments</key>
-          <array>
-              <string>#{opt_bin}/activemonitor</string>
-              <string>rec</string>
-          </array>
-          <key>RunAtLoad</key>
-          <true/>
-          <key>StartInterval</key>
-          <integer>60</integer>
-      </dict>
-      </plist>
-    EOS
-  end
-
   def install
     bin.install "activemonitor"
+  end
+
+  service do
+    run ["#{opt_bin}/activemonitor", "rec"]
+    run_type :interval
+    interval 60
   end
 end
