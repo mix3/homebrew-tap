9f12904d6a469df8f21a55aca8a560fd880e8454
mix3@ｻﾀﾃﾞｰﾅｲﾄﾌｨｰﾊﾞｰ <himachocost333@gmail.com>
2025-06-15 16:01:16 +0900

    add font-s2g-love-nerd-font.rb

---
+++ Casks/font-s2g-love-nerd-font.rb
@@ -0,0 +1,11 @@
+cask 'font-s2g-love-nerd-font' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://raw.githubusercontent.com/mix3/homebrew-tap/644eb121f3c5437b83cb9d8271cf3c90c7a3e548/Casks/S2GLoveFontNerdFont-Regular.ttf',
+      verified: 'raw.githubusercontent.com/mix3/homebrew-tap/'
+  name 'S2Gらぶ'
+  homepage 'http://s2g.jp/'
+
+  font 'S2GLoveFontNerdFont-Regular.ttf'
+end
