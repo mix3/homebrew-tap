b143e0045ec63eaa49789b17f756ce2f8f7efb1b
mix3@ｻﾀﾃﾞｰﾅｲﾄﾌｨｰﾊﾞｰ <himachocost333@gmail.com>
2025-06-15 16:01:06 +0900

    fix font-s2g-love.rb

--- Casks/font-s2g-love.rb
+++ Casks/font-s2g-love.rb
@@ -2,9 +2,10 @@ cask 'font-s2g-love' do
   version :latest
   sha256 :no_check
 
-  url 'http://s2g.jp/font/zip/love.zip'
+  url 'https://raw.githubusercontent.com/mix3/homebrew-tap/644eb121f3c5437b83cb9d8271cf3c90c7a3e548/Casks/S2GLoveFont-Regular.ttf',
+      verified: 'raw.githubusercontent.com/mix3/homebrew-tap/'
   name 'S2Gらぶ'
   homepage 'http://s2g.jp/'
 
-  font 'love/love.ttf'
+  font 'S2GLoveFont-Regular.ttf'
 end
